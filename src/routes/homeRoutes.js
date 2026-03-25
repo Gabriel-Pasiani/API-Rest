946c7aba574c3b09f434b10d26b897d5d970678f
Gabriel-Pasiani <pasianig@gmail.com>
2026-03-25 18:32:27 -0300

    Api rest

---
+++ src/routes/homeRoutes.js
@@ -0,0 +1,8 @@
+import { Router, Router } from 'express'
+import homeController from '../controllers/HomeController'
+
+const router = new Router()
+
+router.get('/', homeController.index)
+
+export default router
